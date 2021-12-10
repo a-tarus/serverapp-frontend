f39d7ebfdfdfa8620a821c4d2066f33f9939a8f1
Amon Tarus <39539658+atarus30@users.noreply.github.com>
2021-12-09 22:57:41 -0500

    Added the state of the data function to the app.component.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of, startWith } from 'rxjs';
+import { DataState } from './enum/data-state.enum';
 import { AppState } from './interface/app-state';
 import { CustomResponse } from './interface/custom-response';
 import { ServerService } from './service/server.service';
@@ -11,11 +12,20 @@ import { ServerService } from './service/server.service';
 })
 export class AppComponent implements OnInit {
   title = 'Server App';
-  
+
   appState$: Observable<AppState<CustomResponse>>;
   constructor(private serverService: ServerService) { }
 
   ngOnInit(): void {
-
+    this.appState$ = this.serverService.servers$
+      .pipe(
+        map(response => {
+          return { dataState: DataState.LOADED_STATE, appData: response }
+        }),
+        startWith({ dataState: DataState.LOADING_STATE }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR_STATE, error })
+        })
+      );
   }
 }
