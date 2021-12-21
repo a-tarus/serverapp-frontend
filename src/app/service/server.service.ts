65d0f0f7db048d5b6f5e23e6214110eada119162
Amon Tarus <39539658+atarus30@users.noreply.github.com>
2021-12-20 20:45:23 -0500

    Updated the server.service.ts file and removed unneeded code from the css file

--- src/app/service/server.service.ts
+++ src/app/service/server.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, Subscriber, throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
 import { Status } from '../enum/status.enum';
 import { CustomResponse } from '../interface/custom-response';
@@ -12,21 +12,21 @@ export class ServerService {
 
   constructor(private http: HttpClient) { }
 
-  servers$ = <Observable<never>>
+  servers$ = <Observable<CustomResponse>>
     this.http.get<CustomResponse>(`${this.apiUrl}/server/list`)
       .pipe(
         tap(console.log),
         catchError(this.handleError)
       );
 
-  save$ = (server: Server) => <Observable<never>>
+  save$ = (server: Server) => <Observable<CustomResponse>>
     this.http.post<CustomResponse>(`${this.apiUrl}/server/save`, server)
       .pipe(
         tap(console.log),
         catchError(this.handleError)
       );
 
-  ping$ = (ipAddress: string) => <Observable<never>>
+  ping$ = (ipAddress: string) => <Observable<CustomResponse>>
     this.http.get<CustomResponse>(`${this.apiUrl}/server/ping/${ipAddress}`)
       .pipe(
         tap(console.log),
@@ -44,7 +44,8 @@ export class ServerService {
               ...response,
               message: response.data.servers
                 .filter(server => server.status === status).length > 0 ? `Servers filtered by 
-              ${status === Status.SERVER_UP ? 'SERVER_UP' : 'SERVER_DOWN'} status` : `No servers of ${status} found`,
+              ${status === Status.SERVER_UP ? 'SERVER_UP' 
+              : 'SERVER_DOWN'} status` : `No servers of ${status} found`,
               data: {
                 servers: response.data.servers
                   .filter(server => server.status === status)
@@ -59,7 +60,7 @@ export class ServerService {
         catchError(this.handleError)
       );
 
-  delete$ = (serverId: number) => <Observable<never>>
+  delete$ = (serverId: number) => <Observable<CustomResponse>>
     this.http.delete<CustomResponse>(`${this.apiUrl}/server/delete/${serverId}`)
       .pipe(
         tap(console.log),
